f4ccdc7df06f6b3bc0e003ea71f2b2f2da6ab324
Sam <ssamsanya3@gmail.com>
2025-06-01 16:00:06 +0300

    dog n person

--- lib/dog.py
+++ lib/dog.py
@@ -12,4 +12,38 @@ APPROVED_BREEDS = [
 ]
 
 class Dog:
-    pass
+    def __init__(self, name="Fido", breed="Corgi"):
+        if type(name) == str and 1 <= len(name) <= 25:
+            if breed in APPROVED_BREEDS:
+                self.name = name  # Use the property setter
+                self.breed = breed # Use the property setter
+            else:
+                print("Breed must be in list of approved breeds.")
+        else:
+            print("Name must be string between 1 and 25 characters.")
+
+    def get_name(self):
+        print("Retrieving name.")
+        return self._name
+
+    def set_name(self, name):
+        if type(name) != str or not (1 <= len(name) <= 25):
+            print("Name must be string between 1 and 25 characters.")
+        else:
+            print(f"Setting name to { name }.")
+            self._name = name
+
+    name = property(get_name, set_name)
+
+    def get_breed(self):
+        print("Retrieving breed.")
+        return self._breed
+
+    def set_breed(self, breed):
+        if breed not in APPROVED_BREEDS:
+            print("Breed must be in list of approved breeds.")
+        else:
+            print(f"Setting breed to { breed }.")
+            self._breed = breed
+
+    breed = property(get_breed, set_breed)
--- lib/person.py
+++ lib/person.py
@@ -16,4 +16,38 @@ APPROVED_JOBS = [
 ]
 
 class Person:
-    pass
+    def __init__(self, name="John Doe", job="Admin"):
+        if type(name) == str and 1 <= len(name) <= 25:
+            if job in APPROVED_JOBS:
+                self.name = name  # Use the property setter
+                self.job = job # Use the property setter
+            else:
+                print("Job must be in list of approved jobs.")
+        else:
+            print("Name must be string between 1 and 25 characters.")
+
+    def get_name(self):
+        print("Retrieving name.")
+        return self._name
+
+    def set_name(self, name):
+        if type(name) != str or not (1 <= len(name) <= 25):
+            print("Name must be string between 1 and 25 characters.")
+        else:
+            print(f"Setting name to { name }.")
+            self._name = name.title()
+
+    name = property(get_name, set_name)
+
+    def get_job(self):
+        print("Retrieving job.")
+        return self._job
+
+    def set_job(self, job):
+        if job not in APPROVED_JOBS:
+            print("Job must be in list of approved jobs.")
+        else:
+            print(f"Setting job to { job }.")
+            self._job = job
+
+    job = property(get_job, set_job)
